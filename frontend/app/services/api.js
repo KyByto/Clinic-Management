ff4855e570d6e7d65856c3c9d9bd92420ac86d67
KyByto <haddadmohamedzakaria12@gmail.com>
2025-06-05 07:55:26 +0100

    logging api url

--- frontend/app/services/api.js
+++ frontend/app/services/api.js
@@ -4,7 +4,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
+console.log('API URL:', API_URL);
 // Instance axios avec configuration de base
 const api = axios.create({
   baseURL: API_URL,
